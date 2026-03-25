68806d6e0beb4cbf3277edead0812477d3bf836b
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-26 00:13:14 +0530

    feat: implement ToolbarPage component

---
+++ frontend/src/pages/ToolbarPage.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import '../styles/pages/ToolbarPage.css';
+
+const ToolbarPage = () => {
+  return (
+    <div>
+      <h1>Toolbar Page</h1>
+      {/* Toolbar component can be imported and used here */}
+    </div>
+  );
+};
+
+export default ToolbarPage;
