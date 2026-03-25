22dfb45c21b1f24f3bac870f84698425aa7a0696
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-26 00:13:12 +0530

    feat: implement DashboardPage component

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import '../styles/pages/DashboardPage.css';
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      {/* Dashboard content goes here */}
+    </div>
+  );
+};
+
+export default DashboardPage;
