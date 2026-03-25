419926d0d640f176a828cb459a7184ad42894dcd
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-26 00:13:13 +0530

    feat: implement SidebarPage component

---
+++ frontend/src/pages/SidebarPage.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import '../styles/pages/SidebarPage.css';
+
+const SidebarPage = () => {
+  return (
+    <div>
+      <h1>Sidebar Page</h1>
+      {/* Sidebar component can be imported and used here */}
+    </div>
+  );
+};
+
+export default SidebarPage;
