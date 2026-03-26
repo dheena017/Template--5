324b8c154dfc7fafef1bef3430bfd46506cc5655
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-26 10:40:33 +0530

    feat: implement PDF Editor page

---
+++ frontend/src/pages/pdf/EditPDF.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PDFTools from '../../components/PDFTools';
+
+const EditPDF = () => {
+    return (
+        <div className="pdf-page-container">
+            <PDFTools initialView="edit-pdf" />
+        </div>
+    );
+};
+
+export default EditPDF;
