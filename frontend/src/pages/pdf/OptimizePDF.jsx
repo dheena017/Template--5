91d4c9d2c11163e6be55f545d40b869f76000c60
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-26 10:40:14 +0530

    feat: implement OptimizePDF page

---
+++ frontend/src/pages/pdf/OptimizePDF.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PDFTools from '../../components/PDFTools';
+
+const OptimizePDF = () => {
+    return (
+        <div className="pdf-page-container">
+            <PDFTools initialView="optimize-pdf" />
+        </div>
+    );
+};
+
+export default OptimizePDF;
