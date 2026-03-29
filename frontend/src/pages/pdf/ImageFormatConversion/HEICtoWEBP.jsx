e746202bc04502ddf9dfe76783cac3602c29e756
dheena017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 18:57:25 +0530

    fix(image): update HEICtoWEBP

--- frontend/src/pages/pdf/ImageFormatConversion/HEICtoWEBP.jsx
+++ frontend/src/pages/pdf/ImageFormatConversion/HEICtoWEBP.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import OrganizePDF from '../OrganizePDF'
-const HEICtoWEBP = () => <OrganizePDF forcedTool="HEIC to WEBP" />
-export default HEICtoWEBP
+import React from 'react';
+import ImageFormatConversionTool from './ImageFormatConversionTool';
+
+const HEICtoWEBP = () => <ImageFormatConversionTool fromFormat="HEIC" toFormat="WEBP" />;
+
+export default HEICtoWEBP;
