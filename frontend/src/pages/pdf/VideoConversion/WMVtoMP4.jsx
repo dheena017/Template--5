9a3047d540dde27edbc5039d08484c985967a082
dheena017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 18:57:26 +0530

    fix(video): update WMVtoMP4

--- frontend/src/pages/pdf/VideoConversion/WMVtoMP4.jsx
+++ frontend/src/pages/pdf/VideoConversion/WMVtoMP4.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import OrganizePDF from '../OrganizePDF'
-const WMVtoMP4 = () => <OrganizePDF forcedTool="WMV to MP4" />
-export default WMVtoMP4
+import React from 'react';
+import VideoConversionTool from './VideoConversionTool';
+
+const WMVtoMP4 = () => <VideoConversionTool fromFormat='WMV' toFormat='MP4' />;
+
+export default WMVtoMP4;
