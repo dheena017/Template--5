9efd33db0e6ce8d4302fd3993ad43ba149489609
dheena017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 18:57:26 +0530

    fix(video): update AVItoWMV

--- frontend/src/pages/pdf/VideoConversion/AVItoWMV.jsx
+++ frontend/src/pages/pdf/VideoConversion/AVItoWMV.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import OrganizePDF from '../OrganizePDF'
-const AVItoWMV = () => <OrganizePDF forcedTool="AVI to WMV" />
-export default AVItoWMV
+import React from 'react';
+import VideoConversionTool from './VideoConversionTool';
+
+const AVItoWMV = () => <VideoConversionTool fromFormat='AVI' toFormat='WMV' />;
+
+export default AVItoWMV;
