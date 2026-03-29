e80a4d955363fc8e0548b89f2504d2056a95bceb
dheena017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 18:57:25 +0530

    fix(image-to-pdf): update BatchPNGToPDF

--- frontend/src/pages/pdf/ImageToPDF/BatchPNGToPDF.jsx
+++ frontend/src/pages/pdf/ImageToPDF/BatchPNGToPDF.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import OrganizePDF from '../OrganizePDF'
-const BatchPNGToPDF = () => <OrganizePDF forcedTool="Convert PNG to PDF in batches" />
-export default BatchPNGToPDF
+import React from 'react';
+import ImageToPDFTool from './ImageToPDFTool';
+
+const BatchPNGToPDF = () => <ImageToPDFTool fromFormat="PNG" isBatch={true} />;
+
+export default BatchPNGToPDF;
