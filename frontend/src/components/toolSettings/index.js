1c761a580a976039657a179920f7b98944ac8add
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 17:08:12 +0530

    feat(settings): export all tool settings panels in index.js

---
+++ frontend/src/components/toolSettings/index.js
@@ -0,0 +1,7 @@
+export { default as ToolSettingsPanel } from './ToolSettingsPanel';
+export { default as MergePDFSettings }    from './MergePDFSettings';
+export { default as SplitPDFSettings }    from './SplitPDFSettings';
+export { default as RemovePagesSettings } from './RemovePagesSettings';
+export { default as ExtractPagesSettings } from './ExtractPagesSettings';
+export { default as ScanToPDFSettings }  from './ScanToPDFSettings';
+export { default as OrganizePDFSettings } from './OrganizePDFSettings';
