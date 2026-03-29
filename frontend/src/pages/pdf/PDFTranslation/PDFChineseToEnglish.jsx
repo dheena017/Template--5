132b96c0fe609cff4fc0d2bdf4b6d0e3808573ae
dheena017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 18:57:25 +0530

    fix(pdf): update PDFChineseToEnglish

--- frontend/src/pages/pdf/PDFTranslation/PDFChineseToEnglish.jsx
+++ frontend/src/pages/pdf/PDFTranslation/PDFChineseToEnglish.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import OrganizePDF from '../OrganizePDF'
-const PDFChineseToEnglish = () => <OrganizePDF forcedTool="PDF Chinese to English" />
-export default PDFChineseToEnglish
+import React from 'react';
+import PDFTranslationTool from './PDFTranslationTool';
+
+const PDFChineseToEnglish = () => <PDFTranslationTool fromLang="Chinese" toLang="English" />;
+
+export default PDFChineseToEnglish;
