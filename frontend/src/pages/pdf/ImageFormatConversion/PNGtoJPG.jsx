142ae7c0bf675340c734d32076bf9bb47174d331
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 17:34:07 +0530

    feat: add PNGtoJPG.jsx for converting PNG images to JPG format in browser

---
+++ frontend/src/pages/pdf/ImageFormatConversion/PNGtoJPG.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ImageFormatConversionTool from './ImageFormatConversionTool';
+const PNGtoJPG = () => <ImageFormatConversionTool fromFormat="PNG" toFormat="JPG" />;
+export default PNGtoJPG;
