f98b041b08018486e9a7c5c93efa16d3ba74d248
dheena017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 18:57:25 +0530

    fix(audio): update WAVtoAC3

--- frontend/src/pages/pdf/AudioConversion/WAVtoAC3.jsx
+++ frontend/src/pages/pdf/AudioConversion/WAVtoAC3.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import OrganizePDF from '../OrganizePDF'
-const WAVtoAC3 = () => <OrganizePDF forcedTool="WAV to AC3" />
-export default WAVtoAC3
+import React from 'react';
+import AudioConversionTool from './AudioConversionTool';
+
+const WAVtoAC3 = () => <AudioConversionTool fromFormat='WAV' toFormat='AC3' />;
+
+export default WAVtoAC3;
