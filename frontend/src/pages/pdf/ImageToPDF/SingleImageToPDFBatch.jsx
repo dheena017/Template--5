7bfcf2a825f7e16c27e645face36647666e457d7
dheena017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 18:57:25 +0530

    fix(image-to-pdf): update SingleImageToPDFBatch

--- frontend/src/pages/pdf/ImageToPDF/SingleImageToPDFBatch.jsx
+++ frontend/src/pages/pdf/ImageToPDF/SingleImageToPDFBatch.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import OrganizePDF from '../OrganizePDF'
-const SingleImageToPDFBatch = () => <OrganizePDF forcedTool="Convert single images to PDF in batches" />
-export default SingleImageToPDFBatch
+import React from 'react';
+import ImageToPDFTool from './ImageToPDFTool';
+
+const SingleImageToPDFBatch = () => <ImageToPDFTool fromFormat="Image" isBatch={false} />;
+
+export default SingleImageToPDFBatch;
