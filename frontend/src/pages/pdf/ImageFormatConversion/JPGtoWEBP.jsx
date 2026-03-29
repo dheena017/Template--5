e7f069497eb4ab5065d8dae24ae4112c5ed896c0
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 17:33:49 +0530

    feat: add JPGtoWEBP.jsx for converting JPG images to WEBP format in browser

---
+++ frontend/src/pages/pdf/ImageFormatConversion/JPGtoWEBP.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ImageFormatConversionTool from './ImageFormatConversionTool';
+const JPGtoWEBP = () => <ImageFormatConversionTool fromFormat="JPG" toFormat="WEBP" />;
+export default JPGtoWEBP;
