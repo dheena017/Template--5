75610894e4b291ac34c655f6a52882505c619cab
dheena017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 18:57:25 +0530

    fix(audio): update WAVtoOGG

--- frontend/src/pages/pdf/AudioConversion/WAVtoOGG.jsx
+++ frontend/src/pages/pdf/AudioConversion/WAVtoOGG.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import OrganizePDF from '../OrganizePDF'
-const WAVtoOGG = () => <OrganizePDF forcedTool="WAV to OGG" />
-export default WAVtoOGG
+import React from 'react';
+import AudioConversionTool from './AudioConversionTool';
+
+const WAVtoOGG = () => <AudioConversionTool fromFormat='WAV' toFormat='OGG' />;
+
+export default WAVtoOGG;
