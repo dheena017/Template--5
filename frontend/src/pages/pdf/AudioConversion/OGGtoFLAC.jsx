3f3d6de9a2f1b9a227f7a7aa1c746860fbb406a6
dheenad017 <dheenadayalan.r.s.139@kalvium.community>
2026-03-29 17:41:54 +0530

    feat: add OGGtoFLAC.jsx for converting OGG audio files to FLAC format in browser

--- frontend/src/pages/pdf/AudioConversion/OGGtoFLAC.jsx
+++ frontend/src/pages/pdf/AudioConversion/OGGtoFLAC.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
-import OrganizePDF from '../OrganizePDF'
-const OGGtoFLAC = () => <OrganizePDF forcedTool="OGG to FLAC" />
-export default OGGtoFLAC
+import React from 'react';
+import AudioConversionTool from './AudioConversionTool';
+
+const OGGtoFLAC = () => <AudioConversionTool fromFormat='OGG' toFormat='FLAC' />;
+
+export default OGGtoFLAC;
